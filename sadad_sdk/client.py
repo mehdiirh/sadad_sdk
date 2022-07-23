cb4f6b26aa121be68463ac07e0dcbbdde56e22aa
Mehdi Rahimi <mehdii.rh78@gmail.com>
2022-07-23 10:09:00 +0430

    [UPDATE] Minor Changes

--- sadad_sdk/client.py
+++ sadad_sdk/client.py
@@ -3,5 +3,13 @@ from sadad_sdk.services import RefundService
 
 class Sadad:
 
-    def __init__(self, VPG_KEY: str, RSA_KEY_LOCATION: str):
-        self.refund = RefundService(VPG_KEY, RSA_KEY_LOCATION)
+    def __init__(self, VPG_KEY: str = None, RSA_KEY_LOCATION: str = None):
+        self.__vpg_key = VPG_KEY
+        self.__rsa_key_location = RSA_KEY_LOCATION
+
+    @property
+    def refund(self) -> RefundService:
+        if not self.__vpg_key or not self.__rsa_key_location:
+            raise AttributeError("Both VPG and RSA keys are required for refund service")
+
+        return RefundService(self.__vpg_key, self.__rsa_key_location)
